3a0432de24542cf4c53ea5186240f3be666517e5
valentin-bardet <72023225+valentin-bardet@users.noreply.github.com>
2022-03-22 10:31:15 +0100

    token

--- reunionou.app/src/app/models/User.php
+++ reunionou.app/src/app/models/User.php
@@ -4,7 +4,7 @@ namespace reu\app\app\models;
 class User extends \Illuminate\Database\Eloquent\Model {
     protected $table      = 'user';
     protected $primaryKey = 'id';
-    protected $fillable= ['id','nom','prenom','mail','sexe','password'];
+    protected $fillable= ['id','nom','prenom','mail','sexe','password','token'];
     public    $incrementing = false;
     public $timestamps = false;
 
