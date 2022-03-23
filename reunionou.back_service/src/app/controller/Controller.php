677db05ea6b9c8ac460562471d7aba39997a66cf
HugoGEORG <hugo.georg57@gmail.com>
2022-03-23 09:15:04 +0100

    fin backoffice

---
+++ reunionou.back_service/src/app/controller/Controller.php
@@ -0,0 +1,151 @@
+<?php
+
+
+namespace reu\back\app\controller;
+
+
+use Firebase\JWT\JWT as JWT;
+use Firebase\JWT\Key as Key;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException ;
+use Firebase\JWT\BeforeValidException;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+use reu\back\app\models\User;
+use reu\back\app\models\Rdv;
+use reu\back\app\models\Participer;
+use reu\back\app\models\Commenter;
+
+use  Illuminate\Support\Str;
+use Respect\Validation\Validator as v;
+
+use reu\back\app\utils\Writer;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+/**
+ * Class LBSAuthController
+ * @package reu\app\app\controller
+ */
+class Controller
+{
+    private $c; //le conteneur de dépendance de l'application
+    
+    public function __construct(\Slim\Container $c){
+        $this->container=$c;
+    }
+
+
+    //USER
+    public function allUsers(Request $req, Response $resp, array $args): Response
+    {
+        $commandes = User::all();
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($commandes));
+        return $resp;
+    }
+
+
+    public function getUser(Request $req, Response $resp, array $args): Response
+    {
+        $id=$args['id'];
+        $user = User::select(['id','nom','prenom','mail','sexe'])
+        ->where('id','=',$id)
+        ->FirstorFail();
+
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($user));
+        return $resp;
+    }
+
+
+
+    public function userAbsent(Request $req, Response $resp, array $args): Response
+    {
+        $commandes = User::select(['id','nom','prenom','mail','sexe','dateConnexion'])
+        ->where('datediff(day, dateConnexion, getdate()','>','10')
+        ->get();
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($commandes));
+        return $resp;
+    }
+
+
+    //RDV
+    public function allRdv(Request $req, Response $resp, array $args): Response
+    {
+        $rdv = Rdv::all();
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($rdv));
+        return $resp;
+    }
+
+    
+
+    public function getRdv(Request $req, Response $resp, array $args): Response
+    {
+        $id=$args['id'];
+        $rdv = Rdv::select(['id','lat','long','libelle_event','libelle_lieu','horraire','date','createur_id'])
+        ->where('id','=',$id)
+        ->FirstorFail();
+
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($rdv));
+        return $resp;
+    }
+
+
+    public function rdvPasse(Request $req, Response $resp, array $args): Response
+    {
+        // SELECT col FROM une_table WHERE col_date <= CURDATE()
+
+        $rdv = Rdv::select(['id','lat','long','libelle_event','libelle_lieu','horraire','date','createur_id'])
+        ->where('date','<=', 'CURDATE()')
+        ->get();
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($rdv));
+        return $resp;
+    }
+
+
+
+    //SUPP
+    public function suppUser(Request $req, Response $resp, array $args): Response
+    {
+        $id=$args['id'];
+        $user = User::where('id','=',$id)
+        ->delete();  
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($user));
+        return $resp;
+    }
+
+    public function suppRdv(Request $req, Response $resp, array $args): Response
+    {
+        $id=$args['id'];
+        $rdv = Rdv::where('id','=',$id)
+        ->delete();
+        $resp = $resp->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $resp->getBody()->write(json_encode($rdv));
+        return $resp;
+    }
+}
+
+
+
+
+
+
+
+
+// Afficher les evenement qui ont eu lieu avant la date d'ajd
+
+
+// All rdv
+// where: dateRdv < dateAjd
+// Quand :
+// date ajd
+// DAYOFYEAR(NOW())
+// <
